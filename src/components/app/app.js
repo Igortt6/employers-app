8dd6dd9d948feb1a896fba79f4be79257299a799
Igor <exemple@gmail.com>
2022-09-29 13:43:47 +0300

    addForm done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,8 @@ class App extends Component {
                 {name: 'Senior B', salary: 5000, increase: false, id: 3}
             ]
         }
+        this.maxId = 4;
+
     }
 
     deleteItem = (id) => {
@@ -27,7 +29,21 @@ class App extends Component {
         })
 
     }      
-    
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
 
     render() {
         return (
@@ -42,7 +58,8 @@ class App extends Component {
                 <EmployeesList 
                     data={this.state.data}
                     onDelete={this.deleteItem}/> 
-                <EmployeesAddForm/>
+                <EmployeesAddForm
+                    onAdd={this.addItem}/>
     
            </div>
         );
