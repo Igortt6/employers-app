54a1dec760be9ce7677bb488c3336fbf8ee9f6e9
Ihor Tkachenko <Igor.tt6@gmail.com>
2023-07-04 10:09:27 +0300

    img info

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -5,7 +5,7 @@ import './search-panel.css'
 //2) записываем туда данные из Input. 
 //3) отправляем локальный term в App. Cинхронизируем с глобальм уже в App.js
 
-class searchPanel extends Component {
+class SearchPanel extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -15,19 +15,19 @@ class searchPanel extends Component {
 
     onUpdateSearchLocal = (e) => {
         const term = e.target.value; //записываем в term значение input
-        this.setState({term}); // изменяем term в state
+        this.setState({ term }); // изменяем term в state
         this.props.onUpdateSearchGlobal(term) // отправляем  term на верх в app.js
     }
 
     render() {
         return (
             <input type="text"
-            className="form-control search-input"
-            placeholder="Найти сотрудника"
-            value={this.state.term}
-            onChange={this.onUpdateSearchLocal}/>
+                className="form-control search-input"
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearchLocal} />
         )
     }
 }
 
-export default searchPanel;
+export default SearchPanel;
